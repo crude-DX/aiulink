0f387607a0f2778b21468d71a3a746d823870d22
Kyoung Su Kim <kimk0611@gmail.com>
2025-09-08 09:56:55 +0000

    feat:Update logo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 export const Icons = {
   logo: (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
     <img
-      src="/aiulink_icon.png"
+      src="/logo.png"
       alt="AIULink Logo"
       style={{
-        transform: 'scaleX(1.8)', // 가로로 20% 늘리기
+        transform: 'scaleX(2)', // 가로로 20% 늘리기
         ...props.style
       }}
       {...props}
